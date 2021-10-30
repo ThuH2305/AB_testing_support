47d9477088eb4fe4c225e582aa93749c75d4c68e
Thu Huynh <thu.huynh2305@gmail.com>
2021-10-31 00:37:53 +0200

    Update abtest.py
    Add pre-charts and table

--- abtest.py
+++ abtest.py
@@ -51,3 +51,82 @@ def plot_norm_dist(ax,mu,std,n=1,with_CI=False,sig_level=0.05,label=None,tail='t
     ax.axvline(mu, c=color, linestyle='--', alpha=0.5)
     if with_CI:
         plot_CI(ax,mu,std,sig_level=sig_level,tail=tail,test_type='z-test')
+
+def plot_vbar_result(ax,data,mu_dict,delta=0.4):
+    yspan = len(data)
+    color = ['#c1daf0','#cccccc']
+    linecolor = ['#428bca','#555555']  
+    yplaces = [.5+i for i in range(yspan)]
+    ylabels = list(data.keys())
+    ax.set_yticks(yplaces,)
+    ax.set_yticklabels(ylabels,fontsize='xx-large')
+    ax.set_ylim(0,yspan)
+    ax.set_facecolor('white')
+    low,hi = data[ylabels[0]]
+    for pos,label in zip(yplaces,ylabels):
+        start,end = data[label]
+        ax.add_patch(patches.Rectangle((start,pos-delta/2.0),end-start,delta))
+        if start<low: low = start #loop for min of range
+        if end>hi: hi= end #loop for max of range
+    t=0
+    for i,rec in zip(ylabels,ax.patches):
+        mu = mu_dict[i]
+        ax.plot([mu,mu],[rec.get_y(),rec.get_y()+rec.get_height()],
+                 linewidth=3,color= linecolor[t])
+        rec.set(facecolor =color[t])
+        ax.text(rec.get_x()+ 0.7*rec.get_width(),
+                rec.get_y()+ 0.2*rec.get_height(),'{}'.format(round(mu,5)),
+               ha='center',va='bottom',fontsize = 'x-large')
+        t+=1
+    ax.plot((low,hi),(0,0))
+    return ax
+
+def plot_show_alpha(ax,mu,std,df,sig_level=0.05,tail='two.sided',color='green',test_type='z-test'):
+    x = np.linspace(mu-8*std,mu+8*std,1000)
+    if test_type =='t-test':
+        left,right = t_test_ci(mu,std,df,tail=tail,sig_level=sig_level) 
+        null = t.pdf(x,df,loc=mu,scale=std)
+    else:
+        left,right = z_test_ci(mu,std,sig_level=sig_level,tail=tail)
+        null = norm(mu,std).pdf(x)
+        
+    if tail=='two.sided':
+        ax.fill_between(x,0,null,color=color,alpha=0.25,where= (x<left) | (x>right))
+    elif tail == 'greater':
+        ax.fill_between(x,0,null,color=color,alpha=0.25,where= (x>right))
+    elif tail == 'less':
+        ax.fill_between(x,0,null,color=color,alpha=0.25,where= (x<left))
+
+def setting_table(sig_level,alternative,labelA,labelB,diff_value):
+    alternative_dict = {'two.sided':'two-sided',
+                        'less':'smaller',
+                        'greater':'larger'}
+    if diff_value != 0 :
+        hypothesis_dict = {'two.sided':'μA ≠ μB',
+                           'two-sided':'μA ≠ μB',
+                       'greater': 'μA > μB + {}'.format(diff_value),
+                       'larger' : 'μA > μB + {}'.format(diff_value),
+                       'smaller': 'μA < μB + {}'.format(diff_value),
+                       'less': 'μA < μB + {}'.format(diff_value)
+                       }
+    else:
+        hypothesis_dict = {'two.sided':'μA ≠ μB',
+                           'two-sided':'μA ≠ μB',
+                       'greater': 'μA > μB',
+                       'larger' : 'μA > μB',
+                       'smaller': 'μA < μB',
+                       'less': 'μA < μB'
+                       }
+        
+    setting_table = """<tr><th>Control Group</th><th>Variation Group</th><th>Alternative Hypothesis</th><th>Significance Level</th></tr>
+                <tr><td>{}</td><td>{}</td><td>{}</td><td>{}</td></tr>""".format(labelA,labelB,hypothesis_dict[alternative],sig_level)
+    main_table_html = """
+    <style>table {width: 100%;}
+    td {text-align: center;}
+    tr:hover {background-color: #f5f5f5;}
+    </style>
+    <center><h3>Test Setting</h3></center>
+    <table>""" +setting_table+ "</table>"
+    display(HTML(main_table_html), metadata=dict(isolated=True))
+
+
